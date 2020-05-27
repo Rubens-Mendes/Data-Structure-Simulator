806abd5608d54d96f39c0798e8173e6d08a19853
Rubens Mendes <rubens_mendes_87@hotmail.com>
2020-05-27 17:38:41 -0300

    Implementação da Simulação de Fila

--- src/pages/Simulacoes/SimuFila/index.js
+++ src/pages/Simulacoes/SimuFila/index.js
@@ -9,24 +9,98 @@ export default class simuFila extends React.Component{
     constructor(props){
         super(props);
         this.state = {
+            max: 0,
+            v: [],
+            i: 0,
+            f: 0,
+            total: 0,
             selectedIndex: -1,
-            textoStep:"Aguardando Operaç?o...",
+            //textoStep:"Aguardando Operaç?o...",
+            texto: "",
         }
 
-        this.updateIndex = this.updateIndex.bind(this)
-        this.operacao = this.updateIndex.bind(this)
-        this.pegaTexto = this.pegaTexto.bind(this)
+        this.updateIndex = this.updateIndex.bind(this);
+        this.operacao = this.operacao.bind(this);
+        this.pegaTexto = this.pegaTexto.bind(this);
+        this.enfileira = this.enfileira.bind(this);
+        this.desenfileira = this.desenfileira.bind(this)
+        this.organizaIndex = this.organizaIndex.bind(this)
+        this.busca = this.busca.bind(this)
+        this.criaFila = this.criaFila.bind(this)
     }
 
     updateIndex (selectedIndex) {
-        this.setState({selectedIndex})
+        this.setState({selectedIndex});
     }
 
     operacao() {
         if(this.state.selectedIndex === 0)
-            this.insercaoLDDE(Number(this.state.texto))
+            this.enfileira(Number(this.state.texto));
         else if(this.state.selectedIndex === 1)
-            this.remocaoLDDE(Number(this.state.texto))
+            this.desenfileira();
+        else if(this.state.selectedIndex === 2)
+            this.busca(Number(this.state.texto));
+        else if(this.state.selectedIndex === 3)
+            this.criaFila(Number(this.state.texto));
+    }
+
+    enfileira(valor){
+        if( (this.state.f+1) % (this.state.max) == this.state.i ){
+            return false;
+        }
+
+        this.setState({total: this.state.total+=1});
+        this.state.v[this.state.f] = {value: valor, index: this.state.total};
+
+        this.state.f = (this.state.f+1) % (this.state.max);
+
+        return true;
+    }
+
+    desenfileira(){
+        if(this.state.i == this.state.f){
+            return this.state.v[0];
+        }
+
+        let temp = this.state.v[this.state.i];
+
+        this.state.v[this.state.i].index = String.fromCharCode(this.state.i+65);
+        this.state.v[this.state.i].value = " ";
+        this.state.i = (this.state.i+1) % (this.state.max);
+        this.setState({total: this.state.total-=1});
+        this.organizaIndex();
+        return temp;
+    }
+
+    organizaIndex(){
+        let tempI = this.state.i;
+
+        while(tempI != this.state.f){
+            this.state.v[tempI].index-=1;
+            tempI = (tempI+1) % (this.state.max);
+        }
+    }
+
+    criaFila(valor){
+        let novo = []
+        for(let i = 0; i < valor; i++){
+            novo[i] = {value: " ", index: String.fromCharCode(i+65)};
+        }
+        this.setState({max: valor, v: novo, i: 0, f: 0, total: 0});
+    }
+
+    busca(valor){
+        let tempI = this.state.i;
+
+        while(tempI != this.state.f){
+            if(this.state.v[tempI].value == valor){
+                alert(String(valor) + " ENCONTRADO NO INDEX " + String(tempI+1) + " DA FILA. ");
+                break;
+            }
+            tempI = (tempI+1) % (this.state.max);
+        }
+
+        alert("VALOR N?O ENCONTRADO.");
     }
 
     pegaTexto(textoDoInput) {
@@ -34,7 +108,7 @@ export default class simuFila extends React.Component{
     }
 
     render(){
-        const buttons = ['Inserir', 'Remover', 'Buscar']
+        const buttons = ['Inserir', 'Remover', 'Buscar', 'Cria Fila']
         const { selectedIndex } = this.state
 
         return(
@@ -59,7 +133,7 @@ export default class simuFila extends React.Component{
                 <View style={Styles.simulationContainer}>
                     <FlatList 
                         horizontal
-                        data={this.state.nodeData}
+                        data={this.state.v}
                         keyExtractor={item => item.index.toString()}
                         renderItem={({ item }) => {
                             return (
